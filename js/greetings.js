6a7a10c276b8c13b3335c3148b775f4f5f71f76a
bokpack <6uiwoom@gmail.com>
2024-03-24 14:47:04 +0900

    240324 login 복습완료

--- js/greetings.js
+++ js/greetings.js
@@ -7,28 +7,28 @@ const USERNAME_KEY = "username"
 
 
 function onLoginSubmit(event) {
-    event.preventDefault(); // 브라우저의 기본동작 막기
-    loginForm.classList.add(HIDDEN_CLASSNAME) // form에 h
-    const username = loginInput.value;
-    localStorage.setItem(USERNAME_KEY,username)
-    paintGreetings(username);
+    event.preventDefault(); 
+    loginForm.classList.add(HIDDEN_CLASSNAME) 
+    const username = loginInput.value; 
+    localStorage.setItem(USERNAME_KEY,username) 
+    paintGreetings(); 
 }
 
-function paintGreetings(username) {
-    greeting.innerText =  `Hello ${username}`
-    greeting.classList.remove(HIDDEN_CLASSNAME)
+function paintGreetings() {
+    const username = localStorage.getItem(USERNAME_KEY);
+    greeting.innerText =  `Hello ${username}` 
+    greeting.classList.remove(HIDDEN_CLASSNAME) 
 
 }
 
 
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
-
 if(savedUsername === null) {
     // show the form
-    loginForm.classList.remove(HIDDEN_CLASSNAME)
-    loginForm.addEventListener("submit",onLoginSubmit)
+    loginForm.classList.remove(HIDDEN_CLASSNAME) 
+    loginForm.addEventListener("submit",onLoginSubmit) 
 } else {
     // show the greetings 
-    paintGreetings(savedUsername)
+    paintGreetings(savedUsername) 
 }
